43b69c30d745b0c689bb0894bad3b359324db5b4
Mounir ZAGHBA <zaghba.mounir@gmail.com>
2019-02-15 07:02:16 +0100

    inject manager

--- src/main/java/export/ExportManager.java
+++ src/main/java/export/ExportManager.java
@@ -1,3 +1,16 @@
 package export;
+
+import org.springframework.stereotype.Component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Component
 public class ExportManager{
+
+  private static final Logger logger = LoggerFactory.getLogger(ExportManager.class);
+
+  public void doExport(){
+    logger.info("***** in ExportManager ******************");
+  }
 }
--- src/main/java/export/ExportScheduler.java
+++ src/main/java/export/ExportScheduler.java
@@ -1,39 +1,48 @@
-
-/*
- * Copyright 2012-2015 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package export;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import java.text.SimpleDateFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ExportScheduler {
 
+    @Autowired
+    private ExportManager exportManager;
+
     private static final Logger log = LoggerFactory.getLogger(ExportScheduler.class);
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() {
-        log.info(" |||||||||||||||||||||||||||||||  The time is now {}", dateFormat.format(new Date()));
+    public void exportFile() {
+        log.info(" --------------------  Begin Export File", dateFormat.format(new Date()));
+        exportManager.doExport();
+        log.info(" --------------------  End Export File", dateFormat.format(new Date()));
     }
+
+
+
+	/**
+	* Returns value of exportManager
+	* @return
+	*/
+	public ExportManager getExportManager() {
+		return exportManager;
+	}
+
+	/**
+	* Sets new value of exportManager
+	* @param
+	*/
+	public void setExportManager(ExportManager exportManager) {
+		this.exportManager = exportManager;
+	}
+
+
 }
